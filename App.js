8ff457ca6f1fdba3069730c72fbab2f542047c91
Mari Noda <marinoda@Victor-and-Maris-Mac-mini.local>
2020-09-09 22:07:51 +0100

    set up reactNavigator

--- App.js
+++ App.js
@@ -1,27 +1,29 @@
-import React, { forwardRef } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import AppBar from './src/components/appbar';
 
-import SignUpScreen from './src/screens/SignUpScreen';
 
-export default class App extends React.Component {
-  render() {
-    return(
-    <View style={styles.container}>
-      <AppBar />
-      <SignUpScreen /> 
-    </View>
-    );
-    }
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MemoListScreen from './src/screens/MemoListScreen';
+
+
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Homme" component={MemoListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'antiquewhite',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 78,
-  },
-});
+export default App;
+
+
+
+
+
 
